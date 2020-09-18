31f87da057f61ee3cd1be5ca56e6aeea172cec85
Guilherme Gps <guilherme.santos@eigmercados.com.br>
2020-09-18 16:51:00 -0300

    Commit original

---
+++ src/test/java/com/apimdb/apimdb/ApimdbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apimdb.apimdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApimdbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
